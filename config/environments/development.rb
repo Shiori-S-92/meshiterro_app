c39239dfc0962a03f54c18397e31aaf41b9fac6c
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-06 13:47:07 +0000

    [Add]ActiveStorageのインストール

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "8563f911224c482e8f750dc8a78dcb9e.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.active_job.queue_adapter = :inline
 end
