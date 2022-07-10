54bc0f3d4ab502a4551f12805a17348c88593138
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-10 14:11:58 +0000

    [Add]4-10／「登録機能」投稿データの保存･ストロングパラメータ

--- app/controllers/post_image_controller.rb
+++ app/controllers/post_image_controller.rb
@@ -4,6 +4,14 @@ class PostImageController < ApplicationController
     @post_image = PostImage.new
   end
 
+  # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
   def index
   end
 
@@ -12,4 +20,12 @@ class PostImageController < ApplicationController
 
   # def edit
   # end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
 end
