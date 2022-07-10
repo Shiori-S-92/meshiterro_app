a3475464b3b2f8705e726bc674cd3dab8ad38d69
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-10 13:38:12 +0000

    [Add]4-9／1:Nのアソシエーション実装,コントローラ用意,resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
 
+  # get 'post_image/new'
+  # get 'post_image/index'
+  # get 'post_image/show'
+  # get 'post_image/edit'
+  resources :post_images, only: [:new, :index, :show]
 
   get 'homes/top'
   get "/homes/about" => "homes#about", as: "about"
