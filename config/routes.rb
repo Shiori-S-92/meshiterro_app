ba10ba11f91e01dc78ebc2affdfb8fc4d4f891d5
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-11 14:15:11 +0000

    [Add,Fix] 4-10／投稿画面,登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   devise_for :users
 
-  # get 'post_image/new'
-  # get 'post_image/index'
-  # get 'post_image/show'
-  # get 'post_image/edit'
-  resources :post_image, only: [:new, :create, :index, :show]
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
+  resources :post_images, only: [:new, :create, :index, :show]
 
-  get 'homes/top'
+  # get 'homes/top'
   get "/homes/about" => "homes#about", as: "about"
 end
