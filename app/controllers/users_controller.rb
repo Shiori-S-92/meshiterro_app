817eec573fe584890daa6ef5f61eb1b5e3454a4d
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-13 13:37:45 +0000

    [Add]4-15／Controller・View => showの記述追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
+    # URLに記載されたIDを参考に、必要なUserモデルを取得する
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
   end
 
   def edit
