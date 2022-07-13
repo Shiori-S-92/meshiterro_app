3066ff56d580c34b8400746c883bbc1721d134ab
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-13 13:06:41 +0000

    [Add] 4-14／usersコントローラ作成, ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
+
   devise_for :users
+  # get 'homes/top'
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
 
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
-  # get 'homes/top'
-  get "/homes/about" => "homes#about", as: "about"
+  # get 'users/show'
+  # get 'users/edit'
+  resources :users, only: [:show, :edit]
+
 end
